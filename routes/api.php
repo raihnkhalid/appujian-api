23703e64acebe09180cecc6bae2eb0d87a1ef0a6
raihnkhalid <raihnkhalid@gmail.com>
2023-01-15 16:55:20 +0700

    Edit route

--- routes/api.php
+++ routes/api.php
@@ -23,35 +23,35 @@ use Illuminate\Support\Facades\Artisan;
 // Route Group for valid access token only
 Route::group(['middleware' => 'auth:sanctum'], function() {
     // Route for create, update, and delete data siswa.
-    Route::post('/siswa/insert', [SiswaController::class, 'store']);
-    Route::post('/siswa/update/{user_id}', [SiswaController::class, 'update']);
-    Route::get('/siswa/delete/{user_id}', [SiswaController::class, 'destroy']);
+    Route::post('/siswa/insert', [SiswaController::class, 'store']); //done doc
+    Route::post('/siswa/update/{user_id}', [SiswaController::class, 'update']); //done doc
+    Route::get('/siswa/delete/{user_id}', [SiswaController::class, 'destroy']); //done doc
 
     // Route for create, update, and delete data kelas.
-    Route::post('/kelas/insert', [KelasController::class, 'store']);
-    Route::post('/kelas/update/{kelas_id}', [KelasController::class, 'update']);
-    Route::post('/kelas/delete/{kelas_id}', [KelasController::class, 'destroy']);
+    Route::post('/kelas/insert', [KelasController::class, 'store']); //done doc
+    Route::post('/kelas/update/{kelas_id}', [KelasController::class, 'update']); //done doc
+    Route::get('/kelas/delete/{kelas_id}', [KelasController::class, 'destroy']); //done doc
 
     // Route for get user/siswa data and logout.
-    Route::get('/user/logout', [AuthController::class, 'logout']);
-    Route::get('/users', [SiswaController::class, 'getUsersData']);
-    Route::get('/user', [SiswaController::class, 'getUserData']);
+    Route::get('/user/logout', [AuthController::class, 'logout']); //done doc
+    Route::get('/users', [SiswaController::class, 'getUsersData']); //done doc
+    Route::get('/user', [SiswaController::class, 'getUserData']); //done doc
 
     // Route for create, update, and delete data ruangan.
     Route::post('/ruangan/insert', [RuanganController::class, 'store']);
     Route::post('/ruangan/update/{ruangan_id}', [RuanganController::class, 'update']);
-    Route::post('/ruangan/delete/{ruangan_id}', [RuanganController::class, 'destroy']);
+    Route::get('/ruangan/delete/{ruangan_id}', [RuanganController::class, 'destroy']);
 });
 
 // Route for register and login admin, for register route can only be used once. If there already exist admin accoynt, this route cannot be used
-Route::post('/admin/register', [AuthController::class, 'registerAdmin'])->middleware('restrictors');
-Route::post('/admin/login', [AuthController::class, 'adminLogin']);
+Route::post('/admin/register', [AuthController::class, 'registerAdmin'])->middleware('restrictors'); //done doc
+Route::post('/admin/login', [AuthController::class, 'adminLogin']); //done doc
 
 // Route for user/siswa login
-Route::post('/user/login', [AuthController::class, 'login']);
+Route::post('/user/login', [AuthController::class, 'login']); //done doc
 
 // Route for get kelas data
-Route::get('/kelas', [KelasController::class, 'show']);
+Route::get('/kelas', [KelasController::class, 'show']); //done doc
 Route::get('/ruangan', [RuanganController::class, 'show']);
 
 Route::get('/clear-cache', function() {
